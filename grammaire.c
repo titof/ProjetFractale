1415350483ddaae2a26bff12a3f77278aec3c071
Fabien Breuil <fabienbreuil5@gmail.com>
2013-10-31 12:28:33 +0100

    changement du nom de grammaire

---
+++ grammaire.c
@@ -0,0 +1,52 @@
+#include <stdio.c>
+#include <stdio.h>
+#include "Grammaire.h"
+
+
+
+void grammaire_lire(char *nomfic, Grammaire *g){
+
+  FILE *pfo ;
+  pfo = fopen((nomfic), "r");
+  
+  if(!pfo) {
+    printf("Ouverture du fichier %s impossible\n", nomfic);
+    exit(2);
+  }
+  /* lecture nom de a fractale */
+  fscanf(pfo, "%s\n",g->titre);
+  /* lecture nombre de directions */
+  fscanf(pfo,"%d\n",&(g->turtle_dir_max));
+  /* lecture de la direction initiale */
+  fscanf(pfo,"%d\n",&(g->turtle_dir_init));
+  /* lecture de l'axiome */
+  fscanf(pfo,"%s\n",g->axiome);
+  /* aucune regles au depart */
+  g->nb_dir = 0;
+  while (fscanf(pfo, "%c ->%s\n", &(g->car[g->nb_dir]), g->regle[g->nb_dir]) != (EOF))
+    { 
+      
+      /* calcul de la longueur de la partie droite de la regle */
+      g->long_regle[g->nb_dir] = strlen(g->regle[g->nb_dir]);
+      g->nb_dir++;
+    }
+  fclose(pfo);
+}
+
+void grammaire_afficher(Grammaire g)
+{
+  int i = 0;
+  
+  printf("Nom de la grammaire : %s\n", g.titre);
+  printf("Nombre de directions : %d\n", g.nb_dir);
+  printf("Direction initiale : %d\n", g.turtle_dir_init);
+  printf("Axiome : s\n", g.axiome);
+  
+  while (i < 10
+         && strcmp(&g.car[i], ""))
+    {
+      printf("Regle de production %d : ", i + 1);
+      printf("%c->%s\n", g.car[i], g.regle[i]);
+      i++;
+    }
+}
---
+++ grammaire.h
@@ -0,0 +1,23 @@
+#ifndef GRAMMAIRE_H_INCLUS
+#defint GRAMMAIRE_H_INCLUS
+
+#define CHMAX 80 /* Dimension maximum d'une ligne du fichier */
+#define NUMMAX 10 /* Nombre max de règles de production */
+
+/* Grammaire */ 
+typedef struct{
+  char nom[CHMAX]; /* Nom de la fractale courante */
+  char axiome[CHMAX]; /* Axiome */
+  int num; /* Nombre de règles de production */
+  char regle[NUMMAX][CHMAX-3]; /* Tableau des parties droites des règles */
+  char car[NUMMAX]; /* Tableau des parties gauches des règles */
+  int long_regle[NUMMAX]; /* Longueur des parties droites des règles */
+  int turtle dir_max; /* Nombre de directions possibles pour la tortue */
+  int turtle dir_init; /* Direction initiale */
+}Grammaire;
+
+void grammaire_lire(char *nomfic, Grammaire *g);
+void grammaire_afficher(Grammaire g);
+int grammaire_trouver(Grammaire *g, char cmd);
+
+#endif
